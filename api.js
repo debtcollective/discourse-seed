241f5816dd271a92cd1b5a23f88a2cc98077386d
Alexandre Marcondes <alex@marcondesian.net>
2018-03-04 17:36:47 -0500

    Simplify variable gathering

--- api.js
+++ api.js
@@ -1,11 +1,7 @@
 const sa = require('superagent').agent();
 const { collectives } = require('./seed');
 
-const {
-  DISCOURSE_API_KEY: api_key = '',
-  DISCOURSE_API_USERNAME: api_username = 'system',
-  DISCOURSE_URL = 'http://localhost:3000',
-} = process.env;
+const { api_key = '', api_username = 'system', api_url = 'http://localhost:3000' } = require('./secrets');
 
 const noNulls = (exports.noNulls = obj => {
   Object.keys(obj).forEach(k => {
@@ -18,7 +14,7 @@ const noNulls = (exports.noNulls = obj => {
 
 const getExistingCollectives = (exports.getExistingCollectives = () =>
   sa
-    .get(DISCOURSE_URL + '/categories.json')
+    .get(api_url + '/categories.json')
     .query({ api_key, api_username })
     .then(({ body: { category_list: { categories } } }) => {
       const categoryNames = Object.keys(collectives).map(k => collectives[k].collective);
@@ -27,19 +23,19 @@ const getExistingCollectives = (exports.getExistingCollectives = () =>
 
 const getExistingGroups = (exports.getExistingGroups = () =>
   sa
-    .get(DISCOURSE_URL + '/groups/search.json')
+    .get(api_url + '/groups/search.json')
     .query({ api_key, api_username })
     .then(({ body: groups }) => groups));
 
 const getExistingTagGroups = (exports.getExistingTagGroups = () =>
   sa
-    .get(DISCOURSE_URL + '/tag_groups.json')
+    .get(api_url + '/tag_groups.json')
     .query({ api_key, api_username })
     .then(({ body: { tag_groups } }) => tag_groups));
 
 const createCollective = (exports.createCollective = (name, text_color = 'black', color = 'red') =>
   sa
-    .post(DISCOURSE_URL + '/categories.json')
+    .post(api_url + '/categories.json')
     .field({
       name,
       text_color,
@@ -51,7 +47,7 @@ const createCollective = (exports.createCollective = (name, text_color = 'black'
     .catch(err => {}));
 
 const createGroup = (exports.createGroup = name =>
-  sa.post(DISCOURSE_URL + '/admin/groups').field({
+  sa.post(api_url + '/admin/groups').field({
     'group[name]': name,
     api_key,
     api_username,
@@ -59,13 +55,13 @@ const createGroup = (exports.createGroup = name =>
 
 const updateCollective = (exports.updateCollective = collective =>
   sa
-    .put(DISCOURSE_URL + '/categories/' + collective.id)
+    .put(api_url + '/categories/' + collective.id)
     .field(noNulls(collective))
     .then(({ body: { category } }) => category));
 
 const updatePost = (exports.updatePost = (id, raw) =>
   sa
-    .put(DISCOURSE_URL + '/posts/' + id)
+    .put(api_url + '/posts/' + id)
     .field({
       'post[raw]': raw,
       api_key,
@@ -75,7 +71,7 @@ const updatePost = (exports.updatePost = (id, raw) =>
 
 const updateTopic = (exports.updateTopic = ({ slug, id }, title) =>
   sa
-    .put(DISCOURSE_URL + '/t/' + slug + '/' + id + '.json')
+    .put(api_url + '/t/' + slug + '/' + id + '.json')
     .field({
       title,
       api_key,
@@ -84,7 +80,7 @@ const updateTopic = (exports.updateTopic = ({ slug, id }, title) =>
     .then(({ body }) => body));
 
 const updateTagGroup = (exports.updateTagGroup = ({ id, name, tag_names }) => {
-  const req = sa.put(DISCOURSE_URL + '/tag_groups/' + id + '.json').field({ name, api_key, api_username });
+  const req = sa.put(api_url + '/tag_groups/' + id + '.json').field({ name, api_key, api_username });
 
   tag_names.forEach(tn => req.field('tag_names[]', tn));
 
@@ -92,7 +88,7 @@ const updateTagGroup = (exports.updateTagGroup = ({ id, name, tag_names }) => {
 });
 
 const createTagGroup = (exports.createTagGroup = ({ name, tag_names }) => {
-  const req = sa.post(DISCOURSE_URL + '/tag_groups.json').field({ name, api_key, api_username });
+  const req = sa.post(api_url + '/tag_groups.json').field({ name, api_key, api_username });
 
   tag_names.forEach(tn => req.field('tag_names[]', tn));
 
@@ -102,7 +98,7 @@ const createTagGroup = (exports.createTagGroup = ({ name, tag_names }) => {
   });
 });
 
-const getPost = (exports.getPost = id => sa.get(DISCOURSE_URL + '/posts/' + id + '.json').then(({ body }) => body));
+const getPost = (exports.getPost = id => sa.get(api_url + '/posts/' + id + '.json').then(({ body }) => body));
 
 const getCollectiveTopicPost = (exports.getCollectiveTopicPost = collective =>
   getCollectiveAboutTopic(collective).then(getTopicFirstPost));
@@ -110,4 +106,4 @@ const getCollectiveTopicPost = (exports.getCollectiveTopicPost = collective =>
 const getTopicFirstPost = (exports.getTopicFirstPost = ({ post_stream: { posts: [{ id }] } }) => getPost(id));
 
 const getCollectiveAboutTopic = (exports.getCollectiveAboutTopic = collective =>
-  sa.get(DISCOURSE_URL + '/t/' + collective.topic_url.split('/').slice(-1)[0] + '.json').then(({ body }) => body));
+  sa.get(api_url + '/t/' + collective.topic_url.split('/').slice(-1)[0] + '.json').then(({ body }) => body));
--- seed.sh
+++ seed.sh
@@ -3,7 +3,5 @@
 ENV=$1
 
 sudo apt-get install awscli
-aws s3 cp s3://tdc-secure/tdc-discourse-seed/secrets.$ENV.sh secrets.sh
-chmod +x ./secrets.sh
-./secrets.sh
+aws s3 cp s3://tdc-secure/tdc-discourse-seed/secrets.$ENV.js secrets.js
 yarn seed
