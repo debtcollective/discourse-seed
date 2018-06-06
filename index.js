ed1464a6b2708dbb67a04db4377be2305c0ca55c
Shoshana Berleant <sberleant@gmail.com>
2018-06-05 21:20:08 -0700

    use rate limiting node api

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const { api_key = '', api_username = 'system', api_url = 'http://localhost:3000'
 /**
  * @type {discourseApi.DiscourseApi}
  */
-const discourse = require('discourse-node-api')({ api_key, api_username, api_url });
+const discourse = require('discourse-node-api')({
+  api_key,
+  api_username,
+  api_url,
+  useRateLimiter: true,
+  sleepSeconds: 1,
+});
 
 async function main() {
   await discourse.admin.settings.enableTags(true);
